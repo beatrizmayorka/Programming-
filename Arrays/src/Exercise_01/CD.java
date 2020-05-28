437176bcd6d196b23ecf42f8c60cd1cb38067209
Beatriz Mayorka de Aguiar <47663726+beatrizmayorka@users.noreply.github.com>
2020-05-28 00:13:01 -0300

    Array exercise created

---
+++ Arrays/src/Exercise_01/CD.java
@@ -0,0 +1,19 @@
+package Exercise_01;
+
+public class CD {
+	String artista;
+	String nome;
+	double preco;
+	
+	public CD(String n, int p, String a) {
+		nome = n; 
+		preco = p; 
+		artista = a; 
+	}
+	
+	public double compraCD(double quantidade) {
+		double valCompra = (this.preco * quantidade); 
+		return valCompra;
+	}
+
+}
